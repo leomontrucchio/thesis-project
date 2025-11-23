fd3394dbf55e88498c7b81993ede121f3f280897
Leonardo Montrucchio <leo.montrucchio@gmail.com>
2025-11-23 17:49:04 +0100

    Add: script for unified inference framework

---
+++ infer_loco.sh
@@ -0,0 +1,8 @@
+export CUDA_VISIBLE_DEVICES=0
+
+class_names=("breakfast_box" "juice_bottle" "pushpins" "screw_bag" "splicing_connectors")
+
+for class_name in "${class_names[@]}"
+    do
+        python3 infer_loco.py --class_name $class_name
+    done
